c4e773d3599da97ccc7d329c3790a2b9069966ef
Gautam Gupta <gautamgupta986047@gmail.com>
2025-04-21 13:08:43 +0530

    Update and rename pra3(k).txt to kthsmallest.c

--- kthsmallest.c
+++ kthsmallest.c
@@ -78,3 +78,66 @@ void main() {
     printf("\nTime taken: %f",timetaken);
     getch();
 }
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int A[], int l, int h) {
+    int pivot = A[l];
+    int i = l;
+    int j = h;
+
+    while (i < j) {
+        do {
+            i++;
+        } while (A[i] < pivot);
+
+        do {
+            j--;
+        } while (A[j] > pivot);
+
+        if (i < j)
+            swap(&A[i], &A[j]);
+    }
+    swap(&A[l], &A[j]);
+    return j;
+}
+
+int kthSmallest(int A[], int l, int h, int k) {
+    if (l <= h) {
+        int pos = partition(A, l, h + 1);
+
+        if (pos == k - 1)
+            return A[pos];
+        else if (pos > k - 1)
+            return kthSmallest(A, l, pos - 1, k);
+        else
+            return kthSmallest(A, pos + 1, h, k);
+    }
+    return -1; 
+}
+
+void main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int A[n];
+
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
+
+    printf("Enter k (for k-th smallest element): ");
+    scanf("%d", &k);
+
+    int result = kthSmallest(A, 0, n - 1, k);
+    if (result != -1)
+        printf("The %d-th smallest element is: %d\n", k, result);
+    else
+        printf("Invalid input!\n");
+}
+
